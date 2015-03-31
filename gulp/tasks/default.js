30d96a139150bf9ab78f0443f071759a04f99003
Adam Pash <adam.pash@gmail.com>
2015-03-31 10:22:36 -0400

    removed tdd b/c not using

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,4 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['sass', 'images', 'markup', 'watch', 'tdd']);
+// gulp.task('default', ['sass', 'images', 'markup', 'watch', 'tdd']);
+gulp.task('default', ['sass', 'images', 'markup', 'watch']);
